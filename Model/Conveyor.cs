8a9bd820f28a1e4ea5fd3fdf86053ffe18d7ff51
Greg Bahns <greg@bahns.com>
2016-01-31 20:36:47 -0500

    Added Conveyor and Droid models.

---
+++ Model/Conveyor.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+	public class Conveyor : IBoardObject
+	{
+		public Position position { get; set; }
+		public Direction direction { get; set; }
+		public int speed;
+	}
+}
---
+++ Model/Droid.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+	public class Droid : IBoardObject
+	{
+		public Position position { get; set; }
+		public Direction direction { get; set; }
+		public DroidColor color { get; set; }
+	}
+}
